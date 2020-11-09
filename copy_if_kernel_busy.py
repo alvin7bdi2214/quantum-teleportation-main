e91f300c6c4a6619ca09175c12280ea2d8b2cd9c
alvin <7bdi2214@mail.u-tokai.ac.jp>
2020-11-09 14:56:35 +0900

    added return but loop doesnt work

--- copy_if_kernel_busy.py
+++ copy_if_kernel_busy.py
@@ -1,18 +1,82 @@
 import random
-def randomnumbers():
-    r = 0
-    r = random.randrange(0,5)
-    return r
+import numpy as np
 
-abc = ["a", "b", "c", "d", "e"]
+# Function to create the random binary string 
+def rand_key(p): 
+	
+	# Variable to store the string 
+	key1 = []
 
-List = []
-while True:
-    rand_number = randomnumbers()
-    List.append(abc[rand_number])
-    if rand_number <3:
-        break
+	# Loop to find the string of desired length 
+	for i in range(p): 
+		
+		# randint function to generate 
+		# 0, 1 randomly and converting the result into str 
+		temp = str(random.randint(0,1)) 
+
+		# Concatenation the random 0, 1 to the final result 
+		key1 += temp 
+		
+	return(key1)
+
+# Driver Code 
+n = 8
+str1 = rand_key(n) 
+print("Desired length random binary string is: ", str1)
+
+from operator import itemgetter
+
+equal_to = {"a":"u_plus", "b":"u_minus", "c":"v_plus", "d":"v_minus"}
 
 
-for i in List:
-    print (i)
+def find_epr_state():
+    
+    sample_letters = 'abcd'
+    count = 0
+    tmplist2 = []
+
+    while True:
+        attempt = random.choice(sample_letters)
+        tmplist2.append(attempt)
+
+        if attempt == "b":
+            break
+
+        count += 1
+
+    return tmplist2
+
+    #print(tmplist2)
+    #print("EPR state occurs after " + str(count) + " attempt(s).")
+
+    #res_list = list(itemgetter(*tmplist2)(equal_to))
+    #print(str(res_list))
+
+#create logic gate
+#group object, use return https://realpython.com/python-return-statement/
+def eval(alice, epr):
+    if (alice == 1 and epr == "a"):
+        return 1  
+    elif (alice == 1 and epr == "b"):
+        return 1 
+    elif (alice == 0 and epr == "c"):
+        return 1 
+    elif (alice == 0 and epr == "d"):
+        return 1
+    elif (alice == 1 and epr == "c"):
+        return 0 
+    elif (alice == 1 and epr == "d"):
+        return 0
+    elif (alice == 0 and epr == "a"):
+        return 0
+    elif (alice == 0 and epr == "b"):
+        return 0
+
+for_bob = []
+
+while n <= 8:
+    rand_key(n)
+    find_epr_state()
+    e1 = eval(str1, find_epr_state())
+    for_bob.append(e1)
+print(for_bob)
