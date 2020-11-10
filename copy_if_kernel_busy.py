fbdd37940faba9a5e5f4ff0d18b9d7ae181e9772
alvin <7bdi2214@mail.u-tokai.ac.jp>
2020-11-10 17:11:41 +0900

    combine loop in copy_if file

--- copy_if_kernel_busy.py
+++ copy_if_kernel_busy.py
@@ -1,39 +1,19 @@
 import random
 import numpy as np
-
-# Function to create the random binary string 
-def rand_key(p): 
-	
-	# Variable to store the string 
-	key1 = []
-
-	# Loop to find the string of desired length 
-	for i in range(p): 
-		
-		# randint function to generate 
-		# 0, 1 randomly and converting the result into str 
-		temp = str(random.randint(0,1)) 
-
-		# Concatenation the random 0, 1 to the final result 
-		key1 += temp 
-		
-	return(key1)
-
-# Driver Code 
-n = 8
-str1 = rand_key(n) 
-print("Desired length random binary string is: ", str1)
-
 from operator import itemgetter
 
-equal_to = {"a":"u_plus", "b":"u_minus", "c":"v_plus", "d":"v_minus"}
-
-
-def find_epr_state():
-    
-    sample_letters = 'abcd'
-    count = 0
-    tmplist2 = []
+equal_to = {"a":"u_plus", "b":"u_minus", "c":"v_plus", "d":"v_minus"} #bell state
+sample_letters = 'abcd' #randomly pick key of bell state dictionary
+count = 0 #count until b is found
+count1 = 0 #count for nested loop
+tmplist2 = [] #list of random sample_letters
+p = 8 #length of generated binary
+key1 = [] #list of generated binary
+for_bob = [] #list of generated binary and bell state through logic gate
+ 
+for i in range(p): 
+	temp = str(random.randint(0,1)) 
+	key1 += temp 
 
     while True:
         attempt = random.choice(sample_letters)
@@ -46,14 +26,7 @@ def find_epr_state():
 
     return tmplist2
 
-    #print(tmplist2)
-    #print("EPR state occurs after " + str(count) + " attempt(s).")
-
-    #res_list = list(itemgetter(*tmplist2)(equal_to))
-    #print(str(res_list))
-
-#create logic gate
-#group object, use return https://realpython.com/python-return-statement/
+#evaluate result of alice binary and bell state
 def eval(alice, epr):
     if (alice == 1 and epr == "a"):
         return 1  
@@ -72,12 +45,10 @@ def eval(alice, epr):
     elif (alice == 0 and epr == "b"):
         return 0
 
-for_bob = []
-t=0
-
-while t <= n:
-    rand_key(t)
-    find_epr_state()
-    e1 = eval(str1, find_epr_state())
-for_bob.append(e1)
+for k in enumerate(key1):
+	for t in enumerate(tmplist2):
+		e = eval(k,t)
+		for_bob.append(e)
+	count1 += 1
+return for_bob
 print(for_bob)
