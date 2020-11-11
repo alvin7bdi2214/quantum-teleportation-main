cf5768322f493c927908db5b6fb676fe96ae6e27
alvin <7bdi2214@mail.u-tokai.ac.jp>
2020-11-11 17:30:33 +0900

    loop worked but printed 1 only

--- copy_if_kernel_busy.py
+++ copy_if_kernel_busy.py
@@ -3,7 +3,6 @@ import numpy as np
 from operator import itemgetter
 
 equal_to = {"a":"u_plus", "b":"u_minus", "c":"v_plus", "d":"v_minus"} #bell state
-sample_letters = 'abcd' #randomly pick key of bell state dictionary
 count = 0 #count until b is found
 count1 = 0 #count for nested loop
 tmplist2 = [] #list of random sample_letters
@@ -13,39 +12,37 @@ for_bob = [] #list of generated binary and bell state through logic gate
  
 for i in range(p): 
 	temp = str(random.randint(0,1)) 
-	key1 += temp 
+	key1.append(temp) 
 
 while True:
-    attempt = random.choice(sample_letters)
+    attempt = str(random.choice(list(equal_to)))
     tmplist2.append(attempt)
 
-    if attempt == "b":
+    if attempt == 'b':
         break
 
     count += 1
 
 #evaluate result of alice binary and bell state
-def eval(alice, epr):
-    if (alice == 1 and epr == "a"):
-        return 1  
-    elif (alice == 1 and epr == "b"):
-        return 1 
-    elif (alice == 0 and epr == "c"):
-        return 1 
-    elif (alice == 0 and epr == "d"):
-        return 1
-    elif (alice == 1 and epr == "c"):
-        return 0 
-    elif (alice == 1 and epr == "d"):
-        return 0
-    elif (alice == 0 and epr == "a"):
-        return 0
-    elif (alice == 0 and epr == "b"):
-        return 0
+def eval(alice, bell):
+    if alice == '1':
+        if bell == 'a' or 'b':
+            return 1
+        elif bell == 'c' or 'd':
+            return 0
+    else:
+        if bell == 'c' or 'd':
+            return 1
+        elif bell == 'a' or 'b':
+            return 0
 
-for k in enumerate(key1):
-	for t in enumerate(tmplist2):
-		e = eval(k,t)
+for k in key1:
+	for t in tmplist2:
+		e = str(eval(k, t))
 		for_bob.append(e)
 	count1 += 1
-for_bob
+
+#tr = [[eval(k,t) for t in tmplist2] for k in key1] #list comprehension split the key properly
+print("generated random binary strings:", key1)
+print("generated bell states:", tmplist2)
+print(for_bob)
