daf15005c7ed64fd620b094558e3ff0fbfe936a0
planargon_pc <pinokioblabla@gmail.com>
2020-11-14 21:31:25 +0900

    b-> epr naming

--- copy_if_kernel_busy.py
+++ copy_if_kernel_busy.py
@@ -1,9 +1,7 @@
 import random
 import numpy as np
-from operator import itemgetter
-import sys
 
-equal_to = {"a":"u_plus", "b":"u_minus", "c":"v_plus", "d":"v_minus"} #bell state
+equal_to = {"a":"u_plus", "EPR":"u_minus", "c":"v_plus", "d":"v_minus"} #bell state
 
 p = 8 #length of generated binary
 
@@ -18,20 +16,20 @@ for i in range(p):
         attempt = str(random.choice(list(equal_to)))
         tmplist2.append(attempt)
 
-        if attempt == 'b':
+        if attempt == 'EPR':
             break
 
 #evaluate result of alice binary and bell state
 def eva(alice, bell):
     if alice == 1:
-        if bell == 'a' or bell == 'b':
+        if bell == 'a' or bell == 'EPR':
             return 1
         elif bell == 'c' or bell == 'd':
             return 0
     elif alice == 0:
         if bell == 'c' or bell == 'd':
             return 1
-        elif bell == 'a' or bell == 'b':
+        elif bell == 'a' or bell == 'EPR':
             return 0
 
 
@@ -40,8 +38,8 @@ _tmp = tmplist2[:]
 
 for k in key1:
     while _tmp:
-        if _tmp[:1] != ['b']:
-            for_bob.append(eva(k, *_tmp[:1]))
+        if _tmp[:1] != ['EPR']:
+            for_bob.append(eva(k, _tmp[0]))
             _tmp = _tmp[1:]
         else:
             for_bob.append(eva(k, *_tmp[:1]))
@@ -50,11 +48,11 @@ for k in key1:
 
 
 #tr = [[eva(k,t) for t in tmplist2] for k in key1] #list comprehension split the key properly
-print("generated random binary strings:", key1)
-print("generated bell states:", tmplist2)
+print("random binary strings:", key1)
+print("bell states:", tmplist2)
 print("encrypted strings:", for_bob)
 
 with open('for_bob.txt', 'w') as f:
-    print("generated random binary strings:\n", key1, file=f)
-    print("generated bell states:\n", tmplist2, file=f)
+    print("random binary strings:\n", key1, file=f)
+    print("random bell states:\n", tmplist2, file=f)
     print("encrypted strings:\n", for_bob, file=f)
