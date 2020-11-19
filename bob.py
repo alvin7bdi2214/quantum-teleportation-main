8b81fa621e3bdc3290eddfeda28cbe2342e0940c
alvin <7bdi2214@mail.u-tokai.ac.jp>
2020-11-19 17:37:58 +0900

    read p4 file

--- bob.py
+++ bob.py
@@ -12,6 +12,82 @@ print(list(itemgetter(*epr_out)(for_bob)))
 #             return list.__getitem__(self, keys)
 #         return [self[k] for k in keys]
 
-from PIL import Image
+
+
+import re
 import numpy
+
+def pbm2numpy(filename):
+    fin = None
+    debug = True
+
+    try:
+        fin = open(filename, "r")
+
+        while True:
+            header = fin.readline().strip()
+            if header.startswith('#'):
+                continue
+            elif header == 'P1':
+                break
+            elif header == 'P4':
+                assert False, 'Raw PBM reading not implemented yet.'
+            else:
+                if debug:
+                    print("Bad mode:", header)
+                return None
+
+        rows, cols = 0, 0
+        while True:
+            header = fin.readline().strip()
+            if header.startswith('#'):
+                continue
+            match = re.match(r'^(\d+) (\+d)$', header)
+            if match == None:
+                if debug:
+                    print("Bad size:", repr(header))
+                return None
+
+            cols, rows = match.groups()
+            break
+
+        rows = int(rows)
+        cols = int(cols)
+
+        assert(rows, cols) != (0, 0)
+
+        if debug:
+            print("Rows: %d, cols: %d", rows, cols)
+
+        result = numpy.zeros((rows,cols), numpy.int8)
+
+        pxs = []
+
+        while True:
+            line = fin.readline().strip()
+            if line == '':
+                break
+
+            for c in line:
+                if c == ' ':
+                    continue
+
+                pxs.append(int(c))
+
+        if len(pxs) != rows*cols:
+            if debug:
+                print("Insufficient image data:", len(pxs))
+            return None
+
+        for r in range(rows):
+            for c in range(cols):
+                result[r,c] = pxs[r*cols + c]
+
+        return result
+
+    finally:
+        if fin != None:
+            fin.close()
+        fin = None
+    return None
 
