995290e31d1a929f25e6d810fae19fc5392c0a40
planargon_pc <pinokioblabla@gmail.com>
2020-11-23 21:50:45 +0900

    generator for generating bell state

--- bob.py
+++ bob.py
@@ -1,11 +1,10 @@
-from copy_if_kernel_busy import tmplist2, for_bob
+from copy_if_kernel_busy import bell_state, for_bob
 from operator import itemgetter
 import math
 
-epr_out = [idx for idx, element in enumerate(tmplist2) if element == 'EPR']
-# print(epr_out)
+epr_idx = [idx for idx, element in enumerate(bell_state) if element == 'EPR']
 
-# print(list(itemgetter(*epr_out)(for_bob)))
+res = list(itemgetter(*epr_idx)(for_bob))
 
 # class Flexlist(list):
 #     def __getitem__(self, keys):
@@ -14,12 +13,32 @@ epr_out = [idx for idx, element in enumerate(tmplist2) if element == 'EPR']
 #         return [self[k] for k in keys]
 
 from PIL import Image
+import random
 im = Image.open("lena_bw.pbm")
+# print(im.format, im.size, im.mode)
 
-def gen_all_coor(l,h):  
-    g = ((x, y) for x in range(l) for y in range(h))
-    for coord in g:
-        print(im.getpixel((coord)))
-    return g
+#using cartesian product to generate all coordinates
+# import itertools
+# def cartesian_prod(cx,cy):
+#     for ix, iy in itertools.product((range(cx), range(cy)))
 
-conv_0_or_1 = lambda c: math.trunc(c/255)
+  
+g = ((x, y) for x in range(im.size[0]) for y in range(im.size[1]))
+cnv = lambda c: math.trunc(c/255)
+for coord in g:
+    c = im.getpixel((coord))
+    cnv(c)
+
+
+for i in range(im.size[0]*im.size[1]):
+    found = False
+    while found == False:
+        x = random.choice(list(equal_to))
+        # print(x)
+
+        if x == 'EPR':
+            found = True
+
+#track memory usage
+import psutil
+print(psutil.virtual_memory())
