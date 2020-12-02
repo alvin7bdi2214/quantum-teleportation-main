af2f40a744d34cd3729da88c192b01c88bfc6d1b
alvin <7bdi2214@mail.u-tokai.ac.jp>
2020-12-02 14:20:03 +0900

    decryption does not work properly

--- bob.py
+++ bob.py
@@ -1,25 +1,34 @@
-from alice import bell_state, for_bob
+from alice import *
 from operator import itemgetter
 import math
+from PIL import Image
 
 cnvTo255 = lambda c2: c2*255
 for_bobDecry = list(map(cnvTo255, for_bob))
 
 epr_idx = [idx for idx, element in enumerate(bell_state) if element == 'EPR']
 print(epr_idx)
-finalResult = list(itemgetter(*epr_idx)(for_bob))
+finalResult = list(itemgetter(*epr_idx)(for_bobDecry))
 
-from PIL import Image
 
 file_name = r"encryptedIMG.pbm"
 
-im = Image.open(file_name)
+img_decr = Image.open(file_name)
 # print(im.format, im.size, im.mode)
+
+with open('decryptedIMG.pbm', 'w') as d:
+    d.write(f'P4\n')
+    i = 0
+    for line in range(im.size[1]):
+        for col in range(im.size[0]):
+            if i < len(finalResult):
+                print(finalResult[i], end=' ', file=d)
+                i += 1
+            else:
+                i = 0
+                print(finalResult[i], end=' ', file=d)
+        print(file=d)
 
 #track memory usage
 # import psutil
 # print(psutil.virtual_memory())
-
-with open('decryptedIMG.pbm', 'w') as d:
-    d.write(f'P4\n')
-    
